17ae11ce58b5762d066581c3f8b492e55a446bcc
Fangyan SUN <zhzxsunfangyan@yeah.net>
2018-12-02 16:17:13 +0100

    add registration and login functionalities
    Signed-off-by: Fangyan SUN <zhzxsunfangyan@yeah.net>

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_mail import Mail
 from config import config
 
@@ -10,6 +11,10 @@ moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+# create_app() function is the application factory
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -19,8 +24,12 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.models import User, Role
 from flask_script import Manager
@@ -14,7 +14,7 @@ manager = Manager(app)
 def make_shell_context():
     return dict(app = app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
+manager.add_command('db', MigrateCommand)
 
 @app.cli.command()
 def test():
