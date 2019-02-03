1241916aa4d3de46af88417f9746b2d104776f86
Fangyan SUN <zhzxsunfangyan@yeah.net>
2019-02-03 16:47:25 +0100

    bug on the api authentication error is identified. "none" is changed to "None"
    Signed-off-by: Fangyan SUN <zhzxsunfangyan@yeah.net>

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,6 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     from .api_1_0 import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1_0')
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     return app
