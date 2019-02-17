c7f6db9a7d45d5abbdd0c56b13471ed479a6ddb0
Fangyan SUN <zhzxsunfangyan@yeah.net>
2019-02-17 16:42:28 +0100

    update

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    mail = Mail()
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
